fc48707e520ed8b5eb802c791092b7d2d4860009
nin0dev <personal@nin0.dev>
2024-10-24 11:14:47 -0400

    feat: load all rest routes

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,16 @@
 import Fastify from "fastify";
+import { readdirSync } from "fs";
+import { prod } from "./utils/constants";
 
-const server = Fastify({
-    logger: process.env.NODE_ENV === "development"
+export const server = Fastify({
+    logger: !prod
 });
+
+const modules = readdirSync(`./${prod ? "dist" : "src"}/rest`);
+for (const module of modules) {
+    server.log.info(`Loading ${module}`);
+    server.register(import(`./rest/${module.replace(".ts", "")}`));
+}
 
 server.listen({ port: 3000 }, (err, address) => {
     if (err) {
