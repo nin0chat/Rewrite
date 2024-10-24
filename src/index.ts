fefbf43266b24b8f6799d7f9a73461e210e9e4c2
nin0dev <personal@nin0.dev>
2024-10-24 11:19:46 -0400

    feat(routes): prefix route with /api/:moduleName

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,9 @@ export const server = Fastify({
 const modules = readdirSync(`./${prod ? "dist" : "src"}/rest`);
 for (const module of modules) {
     server.log.info(`Loading ${module}`);
-    server.register(import(`./rest/${module.replace(".ts", "")}`));
+    server.register(import(`./rest/${module.replace(".ts", "")}`), {
+        prefix: `/api/${module.replace(".ts", "")}`
+    });
 }
 
 server.listen({ port: 3000 }, (err, address) => {
